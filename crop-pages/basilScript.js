4610f1e0c84af99127d08b5f754ec5f035badf29
Ryan <mchldude@outlook.com>
2025-06-21 12:34:44 -0400

    Co-authored-by: kwaaaaaaa <kwaaaaaaa@users.noreply.github.com>

--- crop-pages/basilScript.js
+++ crop-pages/basilScript.js
@@ -1,4 +1,45 @@
+var seasonExpanded = 0;
+var waterExpanded = 0;
+var spaceExpanded = 0;
+
 function expandSeason() {
     const seasonText = document.getElementById("seasonText");
-    seasonText.innerHTML = "Basil is a warm-season herb that grows best in late spring through summer. It should be planted outdoors only after the last frost, when temperatures stay consistently above 10°C. Basil thrives in sunlight and warm weather, with ideal temperatures between 21–32°C. It can be started indoors 4–6 weeks before the last frost for an earlier harvest. The plant is highly sensitive to cold and will not survive frost. In most regions, basil is grown as an annual and harvested until fall."
+    const seasonExpand = document.getElementById("seasonExpand");
+    if (seasonExpanded == 0) {
+        seasonExpanded = 1;
+        seasonExpand.innerHTML = "Hide Growing Season";
+        seasonText.innerHTML = "Basil is a warm-season herb that grows best from late spring through summer. It should be planted after the last frost, when temperatures stay above 10 degrees celsius. Basil thrives in full sun and warm weather between 21-32 degrees celsius. However, it is sensitive to cold, won't survive frost, and is typically grown as an annual.";      
+    } else if (seasonExpanded == 1) {
+        seasonExpanded = 0;
+        seasonExpand.innerHTML = "Growing Season";
+        seasonText.innerHTML = "";
+    }
+}
+
+function expandWater() {
+    const waterText = document.getElementById("waterText");
+    const waterExpand = document.getElementById("waterExpand");
+    if (waterExpanded == 0) {
+        waterExpanded = 1;
+        waterExpand.innerHTML = "Hide Watering";
+        waterText.innerHTML = "";      
+    } else if (waterExpanded == 1) {
+        waterExpanded = 0;
+        waterExpand.innerHTML = "Watering";
+        waterText.innerHTML = "";
+    }
+}
+
+function expandSpace() {
+    const spaceText = document.getElementById("spaceText");
+    const spaceExpand = document.getElementById("spaceExpand");
+    if (spaceExpanded == 0) {
+        spaceExpanded = 1;
+        spaceExpand.innerHTML = "Hide Placement";
+        spaceText.innerHTML = "";      
+    } else if (spaceExpanded == 1) {
+        spaceExpanded = 0;
+        spaceExpand.innerHTML = "Placement";
+        spaceText.innerHTML = "";
+    }
 }
